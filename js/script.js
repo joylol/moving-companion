81b727fbe5308f460b9a4c2d84e98bfd499bdc59
Joy <joylol@users.noreply.github.com>
2015-09-10 12:23:23 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+
+function loadData() {
+
+    var $body = $('body');
+    var $wikiElem = $('#wikipedia-links');
+    var $nytHeaderElem = $('#nytimes-header');
+    var $nytElem = $('#nytimes-articles');
+    var $greeting = $('#greeting');
+
+    // clear out old data before new request
+    $wikiElem.text("");
+    $nytElem.text("");
+
+    // load streetview
+
+    // YOUR CODE GOES HERE!
+    var street = $('#street').val();
+    var city = $('#city').val();
+    var address = street + ', ' + city;
+    console.log(address);
+    $body.append('<img class="bgimg" src="http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '">');
+
+    return false;
+};
+
+$('#form-container').submit(loadData);
